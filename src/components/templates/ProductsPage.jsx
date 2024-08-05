5195e66289d3b029f5346fb197ac1696c43d262a
AmirHossein137 <122604637+AmirHossein137@users.noreply.github.com>
2024-08-05 17:40:26 +0330

    a

--- src/components/templates/ProductsPage.jsx
+++ src/components/templates/ProductsPage.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SideBar from '../modules/SideBar'
 import ProductInfo from '../modules/ProductInfo'
 
 const ProductsPage = () => {
+
+  useEffect(() => {
+    return () => {
+      console.log('cleanup')
+    }
+  },[])
   return (
     <div className='grid grid-cols-12 gap-4'>
       <div className='col-span-2'>
